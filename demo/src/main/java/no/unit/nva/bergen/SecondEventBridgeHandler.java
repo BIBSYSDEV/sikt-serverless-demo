a539f9ba36d7971e89fd86b4a15e10e50c320332
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-08-15 14:05:42 +0200

    Add test coverage for SecondEventBridgeHandler

--- demo/src/main/java/no/unit/nva/bergen/SecondEventBridgeHandler.java
+++ demo/src/main/java/no/unit/nva/bergen/SecondEventBridgeHandler.java
@@ -7,26 +7,35 @@ import no.unit.nva.events.models.AwsEventBridgeEvent;
 import no.unit.nva.events.models.EventReference;
 import no.unit.nva.s3.S3Driver;
 import nva.commons.core.JacocoGenerated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.s3.S3Client;
 
-@JacocoGenerated
 public class SecondEventBridgeHandler extends DestinationsEventBridgeEventHandler<EventReference, Void> {
     
+    private static final Logger logger = LoggerFactory.getLogger(SecondEventBridgeHandler.class);
     private final S3Driver s3Driver;
     
     @JacocoGenerated
     public SecondEventBridgeHandler() {
-        super(EventReference.class);
-        var s3Client = S3Driver.defaultS3Client().build();
-        this.s3Driver = new S3Driver(s3Client,EventsConfig.EVENTS_BUCKET);
+        this(S3Driver.defaultS3Client().build());
     }
     
-    @JacocoGenerated
+    public SecondEventBridgeHandler(S3Client s3Client) {
+        super(EventReference.class);
+        this.s3Driver = new S3Driver(s3Client, EventsConfig.EVENTS_BUCKET);
+    }
+    
     @Override
     protected Void processInputPayload(EventReference input,
                                        AwsEventBridgeEvent<AwsEventBridgeDetail<EventReference>> event,
                                        Context context) {
         var message = s3Driver.readEvent(input.getUri());
-        System.out.println(message);
+        logger.info(formatLogMessage(message));
         return null;
+    }
+    
+    private String formatLogMessage(String message) {
+        return String.format("The message was:%s", message);
     }
 }
